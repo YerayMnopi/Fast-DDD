64067a6aaa38da67adefb8677b0aa1cb478c0de8
Yeray Díaz Rodríguez <yeray.diaz@mongodb.com>
2024-02-07 01:16:46 +0100

    Configure site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yeraymnopi.github.io/',
+  base: '/Fast-DDD',
   integrations: [tailwind()]
 });
